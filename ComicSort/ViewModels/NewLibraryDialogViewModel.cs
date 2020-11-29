57c3fc86a7371226ad92874410117e7415a0731f
Pascal Mc Gregor <pmcgregor@videotron.ca>
2020-11-29 18:16:27 -0500

    Created New Library Dialog
    Created the new library dialog user control

---
+++ ComicSort/ViewModels/NewLibraryDialogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ComicSort.Core.Mvvm;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComicSort.ViewModels
+{
+    public class NewLibraryDialogViewModel : ViewModelBase
+    {
+        public NewLibraryDialogViewModel()
+        {
+
+        }
+    }
+}
---
+++ ComicSort/Views/NewLibraryDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ComicSort.Views
+{
+    /// <summary>
+    /// Interaction logic for NewLibraryDialog
+    /// </summary>
+    public partial class NewLibraryDialog : UserControl
+    {
+        public NewLibraryDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
